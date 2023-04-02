a5590cd2ff32ec6881ae1785ca9e867bcdd7a6d7
oereo <dlstpgns0406@gmail.com>
2023-04-02 17:43:29 +0900

    feat: Product entity class 생성

---
+++ server/database/src/main/java/com/twentiethcenturygangsta/database/domain/Product.java
@@ -0,0 +1,15 @@
+package com.twentiethcenturygangsta.database.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
