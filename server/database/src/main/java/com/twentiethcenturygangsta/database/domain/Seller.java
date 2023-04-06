e37f577fe48eb1790b8385c7d51472860bdcb030
oereo <dlstpgns0406@gmail.com>
2023-04-06 18:09:37 +0900

    feat: Seller, ProductImage entity class 추가

--- server/database/src/main/java/com/twentiethcenturygangsta/database/domain/Seller.java
+++ server/database/src/main/java/com/twentiethcenturygangsta/database/domain/Seller.java
@@ -1,15 +1,32 @@
 package com.twentiethcenturygangsta.database.domain;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Seller {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @OneToMany(mappedBy = "seller", cascade = CascadeType.ALL)
+    private List<Product> products = new ArrayList<>();
+
+    @Column(unique = true)
     private String nickName;
-    private String name;
+    private String userName;
     private int grd;
+
+    @Builder
+    public Seller(String nickName, String userName, int grd) {
+        this.nickName = nickName;
+        this.userName = userName;
+        this.grd = grd;
+    }
 }
