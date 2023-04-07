3ac3be82b2cc79108853f89b30ee668b3ee717ee
Kimkyungmin123 <rud3302@naver.com>
2023-04-07 17:04:58 +0900

    feat: create header component

---
+++ client/src/components/header/index.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import { headerStyles } from './index.css'
+import Button from '../../components/button'
+
+const Header = () => {
+  return (
+    <div className={headerStyles}>
+      <h1>Product</h1>
+    </div>
+  )
+}
+
+export default Header
