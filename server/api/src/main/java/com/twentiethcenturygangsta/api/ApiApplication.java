353af14e67b6d42cc351baeec4dc0054347fbd1a
oereo <dlstpgns0406@gmail.com>
2023-04-11 19:36:22 +0900

    feat: 의존성 모듈 위치 변경 및 ApiApplication 의 componentScan package 범위 설정

--- server/api/src/main/java/com/twentiethcenturygangsta/api/ApiApplication.java
+++ server/api/src/main/java/com/twentiethcenturygangsta/api/ApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.twentiethcenturygangsta.cache-redis")
 @EntityScan(basePackages = "com.twentiethcenturygangsta.database")
 @EnableJpaRepositories(basePackages = "com.twentiethcenturygangsta.database")
 public class ApiApplication {
