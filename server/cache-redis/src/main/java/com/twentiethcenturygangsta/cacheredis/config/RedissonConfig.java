827cec3fa57eb9e89e51dce1d025bb2f399eadc1
oereo <dlstpgns0406@gmail.com>
2023-04-12 20:46:12 +0900

    feat: RedissonConfig 추가

--- server/cache-redis/src/main/java/com/twentiethcenturygangsta/cacheredis/config/RedissonConfig.java
+++ server/cache-redis/src/main/java/com/twentiethcenturygangsta/cacheredis/config/RedissonConfig.java
@@ -3,23 +3,14 @@ package com.twentiethcenturygangsta.cacheredis.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-@Configuration
+@Component
 public class RedissonConfig {
 
     private static final String REDISSON_HOST_PREFIX = "redis://";
 
-    @Value("${spring.data.redis.host}")
-    private String host;
-
-    @Value("${spring.data.redis.port}")
-    private int port;
-
-    @Bean
-    public RedissonClient redissonClient() {
+    public static RedissonClient createRedissonClient(String host, int port) {
         Config config = new Config();
         config.useSingleServer().setAddress(REDISSON_HOST_PREFIX + host + ":" + port);
         return Redisson.create(config);
