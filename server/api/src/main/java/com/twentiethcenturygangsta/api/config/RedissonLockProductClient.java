c6f70265667ba3562b8ceae36c01b94a0b9ad0db
oereo <dlstpgns0406@gmail.com>
2023-04-17 12:43:28 +0900

    feat: RedissonLockProductClient facade class 추가
    - lock 획득 시도 로직

---
+++ server/api/src/main/java/com/twentiethcenturygangsta/api/config/RedissonLockProductClient.java
@@ -0,0 +1,36 @@
+package com.twentiethcenturygangsta.api.config;
+
+import com.twentiethcenturygangsta.api.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class RedissonLockProductClient {
+    private final RedissonClient redissonClient;
+    private final ProductService productService;
+
+    public void decrease(Long id, Long quantity) {
+        RLock lock = redissonClient.getLock(id.toString());
+        try {
+            boolean hasGetLock = lock.tryLock(60, 3, TimeUnit.SECONDS);
+
+            if (!hasGetLock) {
+                log.info("Lock 획득 실패");
+                return;
+            }
+            productService.sellProduct(id, quantity);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+}
